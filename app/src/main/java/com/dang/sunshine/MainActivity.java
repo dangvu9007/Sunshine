d32da25e76fb079056359d7128c76667775fa7d2
dangvu <dangvu9007@gmail.com>
2021-03-12 23:45:40 +0900

    next

--- app/src/main/java/com/dang/sunshine/MainActivity.java
+++ app/src/main/java/com/dang/sunshine/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import java.util.Objects;
+
 import adapter.ListDummyData;
 import fragment.DetailFragment;
 import fragment.ListFragment;
@@ -14,12 +16,22 @@ public class MainActivity extends AppCompatActivity {
 
     private final String TAG_DETAIL = "fragmentDetail";
     private final String TAG_LIST = "fragmentList";
+    private DetailFragment detailFragment;
+    private ListFragment listFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        actionBar();
         addFragment();
+    }
+
+    private void actionBar() {
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic_logo);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
     }
 
     private void addFragment() {
@@ -27,13 +39,11 @@ public class MainActivity extends AppCompatActivity {
         Weather weather = list.getWeatherToDay();
         FragmentManager fragmentManager = getSupportFragmentManager();
 
-        DetailFragment detailFragment = DetailFragment.newInstance(weather,true);
+        detailFragment = DetailFragment.newInstance(weather, true);
         fragmentManager.beginTransaction().add(R.id.fragment_detail, detailFragment, TAG_DETAIL).commit();
 
-        ListFragment listFragment = ListFragment.newInstance();
-        fragmentManager.beginTransaction().add(R.id.frament_list,listFragment,TAG_LIST).commit();
-
-
+        listFragment = ListFragment.newInstance();
+        fragmentManager.beginTransaction().add(R.id.frament_list, listFragment, TAG_LIST).commit();
 
 
     }
